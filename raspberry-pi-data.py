bbd424fe48d139507e3d30df91614390fc56000e
der-ich <knrd-dev@web.de>
2015-05-30 17:38:45 +0200

    Update raspberry-pi-data.py

--- raspberry-pi-data.py
+++ raspberry-pi-data.py
@@ -7,16 +7,21 @@ locale.setlocale(locale.LC_ALL, "")
 
 def freq():
 	"""in Hz"""
-	return locale.format(
-	"%d", int(
-	re.search(r'.*frequency\(\d*\)=(.*)',
-	subprocess.check_output(["vcgencmd", "measure_clock", "arm"]).replace("\n", "")).group(1)),
-	grouping=True) + " Hz"
+	freqstr = subprocess.check_output(["vcgencmd", "measure_clock", "arm"]).replace("\n", "").replace("\r", "")
+	freqstr2 = re.search(r'.*frequency\(\d*\)=(.*)', freqstr).group(1)
+	return locale.format("%d", int(freqstr2), grouping=True) + " Hz"
 		
 def temp():
 	"""in °C """
-	return re.search(r'.*temp=(.*)', subprocess.check_output(["vcgencmd", "measure_temp"]).replace("\n", "")).group(1).replace("'C", "°C")
+	tempstr = subprocess.check_output(["vcgencmd", "measure_temp"]).replace("\n", "").replace("\r", "")
+	return re.search(r'.*temp=(.*)', tempstr).group(1).replace("'C", " °C")
 		
 def volt():
 	"""in Volt"""
-	return re.search(r'.*volt=(.+V)', subprocess.check_output(["vcgencmd", "measure_volts"]).replace("\n", "")).group(1)
+	voltstr = subprocess.check_output(["vcgencmd", "measure_volts"]).replace("\n", "").replace("\r", "")
+	voltstr2 = re.search(r'.*volt=(.*)V', voltstr).group(1)
+	return  str(float(voltstr2)) + " V"
+
+print(freq())
+print(temp())
+print(volt())
